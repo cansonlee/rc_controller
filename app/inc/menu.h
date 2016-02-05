a628bde20187f7460f16cb8fcbfb7880102dfa2e
tusimbe <tusimbe@gmail.com>
2016-02-05 14:29:59 +0800

    adding ui frame

---
+++ app/inc/menu.h
@@ -0,0 +1,6 @@
+#ifndef __MENU_H__
+#define __MENU_H__
+
+void menu_init(void);
+
+#endif
--- drivers/driver_global/driver_global.h
+++ drivers/driver_global/driver_global.h
@@ -1,27 +1,32 @@
 #ifndef __DRIVER_GLOBAL_H
 #define __DRIVER_GLOBAL_H
 
+#define OK          0
+#define ENULLPTR    1
+#define EPARAM      2
+#define ENOMEM      3
+#define ENOSUPPORT  4
+#define EOS         5
 
 
 #define IF_PTR_IS_NULL_RET_NULLPTR_ERR(ptr) \
-		    do \
-		    { \
-		        if (NULL == ptr) \
-		        { \
-		            return -1; \
-		        } \
-		    } \
-		    while(0)
+    do \
+    { \
+        if (NULL == ptr) \
+        { \
+            return -ENULLPTR; \
+        } \
+    } \
+    while(0)
 
 #define IF_CONDITION_TURE_RET_PARAM_ERR(condition) \
-		    do \
-		    { \
-		        if (condition) \
-		        { \
-		            return -1; \
-		        } \
-		    } \
-		    while(0)	
-
+    do \
+    { \
+        if (condition) \
+        { \
+            return -EPARAM; \
+        } \
+    } \
+    while(0)
 
 #endif
